b34855cecfd30b6a4758ce9c29909ed267af26f8
MURAKAMI Masami <masaamura@gmail.com>
2025-02-18 05:54:53 +0900

    Fix history settings

--- dot.zsh.d/0_history.zsh
+++ dot.zsh.d/0_history.zsh
@@ -1,5 +1,7 @@
 ### history
-HISTFILE=~/.zsh_histfile
-HISTORY_IGNORE="(cd|pwd|l[sal])"
-HISTSIZE=10000
-SAVEHIST=10000
+export HISTFILE=~/.zsh_history
+export HISTORY_IGNORE="(cd|pwd|l[sal])"
+export HISTSIZE=10000
+export SAVEHIST=10000
+
+setopt share_history
