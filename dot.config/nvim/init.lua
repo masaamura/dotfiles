ce4a0220dca4a5d79236676708c81c9e4cb794d4
MURAKAMI Masami <masaamura@gmail.com>
2025-02-19 08:26:13 +0900

    Add neovim tabstop settings

--- dot.config/nvim/init.lua
+++ dot.config/nvim/init.lua
@@ -156,6 +156,12 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- [[ masam's options ]]
+-- tabstop
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
