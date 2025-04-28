bfd075d669d32a32a360966332564bddd7359493
MURAKAMI Masami <masaamura@gmail.com>
2025-04-28 18:01:10 +0900

    Change mise method to shims for poetry

--- dot.zsh.d/7_mise.zsh
+++ dot.zsh.d/7_mise.zsh
@@ -1,7 +1,7 @@
 ### mise
 if [[ -x ~/.local/bin/mise ]]; then
-    eval "$(~/.local/bin/mise activate zsh)"
-    #eval "$(~/.local/bin/mise activate --shims)"
+    #eval "$(~/.local/bin/mise activate zsh)"
+    eval "$(~/.local/bin/mise activate --shims)"
 else
     echo "mise is not installed"
 fi
