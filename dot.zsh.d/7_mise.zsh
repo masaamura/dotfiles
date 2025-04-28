68d39afa33c3751a17182478e56432a853c365a3
MURAKAMI Masami <masaamura@gmail.com>
2025-04-29 05:20:20 +0900

    Fixed how mise is found

--- dot.zsh.d/7_mise.zsh
+++ dot.zsh.d/7_mise.zsh
@@ -1,7 +1,7 @@
 ### mise
-if [[ -x ~/.local/bin/mise ]]; then
-    #eval "$(~/.local/bin/mise activate zsh)"
-    eval "$(~/.local/bin/mise activate --shims)"
+if type mise > /dev/null; then
+    #eval "$(mise activate zsh)"
+    eval "$(mise activate --shims)"
 else
     echo "mise is not installed"
 fi
