2ad9e4e92c1acb8e6a1ae8c948fb7fb509eca65a
MURAKAMI Masami <masaamura@gmail.com>
2025-05-12 06:37:34 +0900

    Fix mise setup method

--- dot.zsh.d/7_mise.zsh
+++ dot.zsh.d/7_mise.zsh
@@ -1,7 +1,7 @@
 ### mise
 if type mise > /dev/null; then
-    #eval "$(mise activate zsh)"
-    eval "$(mise activate --shims)"
+    eval "$(mise activate zsh)"
+    #eval "$(mise activate --shims)"
 else
     echo "mise is not installed"
 fi
