2a31713d450b2fb166967a18e44cab812ae505e6
MURAKAMI Masami <masaamura@gmail.com>
2025-08-23 22:16:15 +0900

    Tempolary disable mise on windows

--- dot.zsh.d/7_mise.zsh
+++ dot.zsh.d/7_mise.zsh
@@ -1,7 +1,9 @@
 ### mise
-if type mise > /dev/null; then
-    eval "$(mise activate zsh)"
-    #eval "$(mise activate --shims)"
-else
-    echo "mise is not installed"
+if [ "$(uname -s)" = "Linux" ]; then
+    if type mise > /dev/null; then
+        eval "$(mise activate zsh)"
+        #eval "$(mise activate --shims)"
+    else
+        echo "mise is not installed"
+    fi
 fi
